bccff3c4bed827ea82a0b1992f3f844eb6830172
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-19 22:48:50 +0200

    sidebar completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,12 @@ import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faSpotify,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -23,15 +29,48 @@ const Sidebar = () => (
       >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
+      <NavLink activeclassname="active" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://www.linkedin.com/in/john-prokopos-348427108/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://www.linkedin.com/in/john-prokopos-348427108/"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://github.com/prokoposgiannis"
+        >
+          <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="nonreferrer"
+          href="https://open.spotify.com/artist/3zLaCnaMFxvsiNG4sOv4rn?si=22e2eij2SpOFIRiC6GmUhg"
+        >
+          <FontAwesomeIcon icon={faSpotify} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
