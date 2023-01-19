aad71a8a5f9c93c5ccd7c3d5e897daf79a0f16f2
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-19 19:19:26 +0200

    sidebar initiated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Sidebar />}></Route>
       </Routes>
     </>
   );
