e30662f5589d1991198e12b71e1de8a013b003f0
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-20 21:23:20 +0200

    Home page semi-finished

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
 import "./index.scss";
+import LogoTitle from "../../assets/images/logo-s.png";
 
-const Home = () => "Hello";
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developoer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend developer / Javascript Expert / Musician</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
