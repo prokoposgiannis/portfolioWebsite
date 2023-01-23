e565e32a29135ca301d4c1ea6ef1360cb420be34
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-23 22:59:16 +0200

    About page semicompleted

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,30 @@
 import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
   return (
-    <span>
-      <text>HEY</text>
-    </span>
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+            idx={15}
+          />
+        </h1>
+        <p>Everything is going to be ok</p>
+        <p>At the end.</p>
+        <p>But at the early mid end. Not the late one.</p>
+      </div>
+    </div>
   );
 };
 
