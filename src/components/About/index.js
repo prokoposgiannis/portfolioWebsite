091c4d733802ce34832dfe361f24b47d2869f40d
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-24 20:29:44 +0200

    About page completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,16 @@
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const About = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -16,6 +26,7 @@ const About = () => {
       <div className="text-zone">
         <h1>
           <AnimatedLetters
+            letterClass={letterClass}
             strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
             idx={15}
           />
@@ -23,6 +34,28 @@ const About = () => {
         <p>Everything is going to be ok</p>
         <p>At the end.</p>
         <p>But at the early mid end. Not the late one.</p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
       </div>
     </div>
   );
