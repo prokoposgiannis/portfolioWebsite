9ff251240f15f56f68e6ed2f363256a0b71e30c9
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-26 21:07:14 +0200

    Map done. To be fixed: Map is not fully displayed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import { useState, useEffect, useRef } from "react";
 import "./index.scss";
 import emailjs from "@emailjs/browser";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Contact = () => {
   const [letterClass, setLeeterClass] = useState("text-animate");
@@ -78,6 +79,23 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          John Prokopos,
+          <br />
+          Greece
+          <br />
+          Athens
+          <br />
+          <span>giannisprokopos@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
