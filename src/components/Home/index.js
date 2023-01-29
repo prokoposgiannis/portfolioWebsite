130b0d223cd487860bfb79dec06a61b06cb0280a
prokoposgiannis <giannisprokopos@gmail.com>
2023-01-30 00:06:30 +0200

    test

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-// import Loader from "react-loaders";
 import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
-import Logo from './Logo'
 import './index.scss'
 import Loader from 'react-loaders'
 
